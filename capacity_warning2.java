09465a83a15debd1317a2148c8d15cb957fc2c9e
Raymond <raymondurrutia2@yahoo.com>
2015-08-27 17:25:56 -0400

    first commit

--- capacity_warning2.java
+++ capacity_warning2.java
@@ -1,20 +1,5 @@
 import javax.swing.JOptionPane;
 
-/**
-Raymond Urrutia
-06/18/15
-Programming Assignment 4
-IT 106 - B01
-
-This programs maintains a count of the number of people in a lifeguards section of ocean. 
-The program is required to record the number of people or groups entering or leaving a lifeguards 
-section of ocean at any one time and then display the current number of people in that section.
-If the person or group entering will put the section over capacity, the program is required to 
-display a message informing the entrants they have been denied entry due to capacity restrictions. 
-It is then required to end with a summary report showing how many people are currently in the 
-section.   
-*/
-
 public class CapacityWarning2
 {
    public static void main(String[] args)
